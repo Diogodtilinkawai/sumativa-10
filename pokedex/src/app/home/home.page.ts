5a76fd63335bfc1118fdaa3a3a8348fb129426e1
Donvicio <10143@portalcedi.edu.mx>
2025-01-30 09:50:52 -0600

    .

--- pokedex/src/app/home/home.page.ts
+++ pokedex/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PokeapiService } from '../pokeapi.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,20 @@ import { PokeapiService } from '../pokeapi.service';
 })
 export class HomePage {
   pokemones: any = [];
-  constructor( private pokeservice: PokeapiService) {}
+  constructor( private pokeservice: PokeapiService, private router:Router) {}
   ngOnInit(){
     this.pokeservice.pokemones().subscribe((data: any) => {
       this.pokemones = data.results;
       console.log(data.results);
     });
+  }
+  verDetalle(url: string)
+  {
+    // this.router.navigate(['/detalle']);
+    console.log(url);
+    this.pokeservice.pokemones().subscribe((data: any) => {
+      console.log(data.results.find((element: { url: string; }) => element.url === url));
+    });
+    // return url;
   }
 }
